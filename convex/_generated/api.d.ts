b44381b6bb5d27a3250a1cf8e00f5e36a390fbb5
Yukai Huang <yukaihuangtw@gmail.com>
2026-02-26 11:22:49 -0800

    Implement Inkō MVP plan

---
+++ convex/_generated/api.d.ts
@@ -0,0 +1,57 @@
+/* eslint-disable */
+/**
+ * Generated `api` utility.
+ *
+ * THIS CODE IS AUTOMATICALLY GENERATED.
+ *
+ * To regenerate, run `npx convex dev`.
+ * @module
+ */
+
+import type * as dashboard from "../dashboard.js";
+import type * as decks from "../decks.js";
+import type * as practice from "../practice.js";
+import type * as users from "../users.js";
+import type * as words from "../words.js";
+
+import type {
+  ApiFromModules,
+  FilterApi,
+  FunctionReference,
+} from "convex/server";
+
+declare const fullApi: ApiFromModules<{
+  dashboard: typeof dashboard;
+  decks: typeof decks;
+  practice: typeof practice;
+  users: typeof users;
+  words: typeof words;
+}>;
+
+/**
+ * A utility for referencing Convex functions in your app's public API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = api.myModule.myFunction;
+ * ```
+ */
+export declare const api: FilterApi<
+  typeof fullApi,
+  FunctionReference<any, "public">
+>;
+
+/**
+ * A utility for referencing Convex functions in your app's internal API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = internal.myModule.myFunction;
+ * ```
+ */
+export declare const internal: FilterApi<
+  typeof fullApi,
+  FunctionReference<any, "internal">
+>;
+
+export declare const components: {};
