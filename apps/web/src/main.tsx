09ba1616ab5c06ec21de907cdf11cd9379e7fd0c
Yukai Huang <yukaihuangtw@gmail.com>
2026-02-27 12:27:46 -0800

    chore: pwa

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { registerSW } from "virtual:pwa-register";
 import { App } from "./App";
 import "./i18n";
 import "./styles/theme.css";
@@ -9,10 +10,11 @@ import { applyThemePreferences, loadThemePreferences } from "./theme/theme";
 
 const queryClient = new QueryClient();
 applyThemePreferences(loadThemePreferences());
+registerSW({ immediate: true });
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-      <QueryClientProvider client={queryClient}>
+    <QueryClientProvider client={queryClient}>
       <BrowserRouter basename={import.meta.env.BASE_URL}>
         <App />
       </BrowserRouter>
